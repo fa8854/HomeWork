2b380a0a71890c5ecfb6fb61338abec607d29137
fa8854 <fa8854@yandex.ru>
2023-01-12 16:27:37 +0400

    Home Work

---
+++ src/test/java/my/ovsyannikov/den/homework/HomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package my.ovsyannikov.den.homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
